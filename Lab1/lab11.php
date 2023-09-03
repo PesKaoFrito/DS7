67d00ad9912e07be680f71d22e9eed5215a1f335
PesKaoFrito <oarg2704@gmail.com>
2023-09-03 18:06:50 -0500

    Commit inicial
    Primer avance en el Lab1

---
+++ Lab1/lab11.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>
+            Laboratorio 1.1
+        </title>
+    </head>
+    <body>
+        <?php
+        $ini="Hola ";
+        $fin="a todos";
+        $todo=$ini.$fin;
+        echo $todo;
+        ?>
+    </body>
+</html>
