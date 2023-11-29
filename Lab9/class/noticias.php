2b6db1bf10c94ed38e30878db2362cec3ea0aa44
PesKaoFrito <99690173+PesKaoFrito@users.noreply.github.com>
2023-11-28 19:27:37 -0500

    Version 1.0

---
+++ Lab9/class/noticias.php
@@ -0,0 +1,28 @@
+<?php
+    require_once('modelo.php');
+
+    final class noticias extends modeloCredencialesBD {
+        protected $titulo;
+        protected $texto;
+        protected $categoria;
+        protected $fecha;
+        protected $imagen;
+        public function __construct() {
+            parent::__construct();
+        }
+        public function consultar_noticias() {
+            $instruccion = "CALL sp_listar_noticias ()";
+            $consulta = $this->_db->query($instruccion);
+            $resultado = $consulta->fetch_all(MYSQLI_ASSOC);
+        
+            if (!$resultado) {
+                echo "Fallo al consultar las noticias";
+            } else {
+                $consulta->close();
+                $this->_db->close();
+                return $resultado;
+            }
+        }        
+    }
+    
+?>
---
+++ Lab9/lab91.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Laboratorio 9.1</title>
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+<body>
+    <h1>Consulta de noticias</h1>
+
+    <?php
+    require_once("class/noticias.php");
+    $obj_noticia = new noticias(); // Correct the class name to noticias
+    $noticias = $obj_noticia->consultar_noticias();
+    $nfilas = count($noticias); // Correct the syntax error and assign the count to $nfilas
+
+    if ($nfilas > 0) {
+        echo "<table>\n";
+        echo "<tr>\n";
+        echo "<th>Titulo</th>\n";
+        echo "<th>Texto</th>\n";
+        echo "<th>Categoria</th>\n";
+        echo "<th>Fecha</th>\n";
+        echo "<th>Imagen</th>\n";
+        echo "</tr>\n";
+
+        foreach ($noticias as $resultado) {
+            echo "<tr>\n";
+            echo "<td>" . $resultado['titulo'] . "</td>\n";
+            echo "<td>" . $resultado['texto'] . "</td>\n";
+            echo "<td>" . $resultado['categoria'] . "</td>\n";
+            echo "<td>" . date("j/n/Y", strtotime($resultado['fecha'])) . "</td>\n";
+
+            if ($resultado['imagen'] != "") {
+                echo "<td><a target='_blank' href='img/" . $resultado['imagen'] . "'><img border='0' src='img/iconotexto.gif'></a></td>\n";
+            } else {
+                echo "<td>&nbsp;</td>\n";
+            }
+
+            echo "</tr>\n";
+        }
+
+        echo "</table>\n";
+    } else {
+        echo "No hay noticias disponibles";
+    }
+    ?>
+</body>
+</html>
